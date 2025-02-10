411d8b2b3b53ddf6d2fcab91389e6b191cf8fc84
atp-gh <atp@disroot.org>
2025-02-10 23:28:30 +0800

    feat: Close some vscode extensions.

--- config/vscode.nix
+++ config/vscode.nix
@@ -2,12 +2,12 @@
 {
   programs.vscode = {
     enable = true;
-    package = pkgs.vscodium;
+    package = pkgs.vscodium-fhs;
     extensions = with pkgs; [
       vscode-extensions.alefragnani.project-manager
       vscode-extensions.antfu.icons-carbon
       vscode-extensions.antfu.slidev
-      vscode-extensions.arrterian.nix-env-selector
+      # vscode-extensions.arrterian.nix-env-selector
       vscode-extensions.astro-build.astro-vscode
       vscode-extensions.biomejs.biome
       vscode-extensions.bradlc.vscode-tailwindcss
@@ -18,36 +18,34 @@
       vscode-extensions.denoland.vscode-deno
       vscode-extensions.esbenp.prettier-vscode
       vscode-extensions.fill-labs.dependi
-      vscode-extensions.formulahendry.code-runner
-      vscode-extensions.github.copilot
-      vscode-extensions.github.copilot-chat
-      vscode-extensions.github.vscode-github-actions
-      vscode-extensions.gleam.gleam
-      vscode-extensions.golang.go
+      # vscode-extensions.formulahendry.code-runner
+      # vscode-extensions.github.copilot
+      # vscode-extensions.github.copilot-chat
+      # vscode-extensions.github.vscode-github-actions
+      # # vscode-extensions.gleam.gleam
+      # vscode-extensions.golang.go
       vscode-extensions.jnoortheen.nix-ide
       vscode-extensions.lokalise.i18n-ally
-      vscode-extensions.mkhl.direnv
+      # vscode-extensions.mkhl.direnv
       vscode-extensions.moshfeu.compare-folders
       vscode-extensions.ms-ceintl.vscode-language-pack-zh-hans
-      vscode-extensions.ms-python.debugpy
-      vscode-extensions.ms-python.python
-      vscode-extensions.ms-python.vscode-pylance
-      vscode-extensions.ms-vscode-remote.remote-ssh
-      vscode-extensions.ms-vscode-remote.remote-ssh-edit
-      vscode-extensions.ms-vscode.cpptools-extension-pack
-      vscode-extensions.ms-vscode.hexeditor
+      # vscode-extensions.ms-python.debugpy
+      # vscode-extensions.ms-python.python
+      # vscode-extensions.ms-python.vscode-pylance
+      # vscode-extensions.ms-vscode.cpptools-extension-pack
+      # vscode-extensions.ms-vscode.hexeditor
       vscode-extensions.naumovs.color-highlight
       vscode-extensions.pkief.material-icon-theme
-      vscode-extensions.pylyzer.pylyzer
-      vscode-extensions.redhat.java
-      vscode-extensions.rust-lang.rust-analyzer
-      vscode-extensions.shd101wyy.markdown-preview-enhanced
-      vscode-extensions.streetsidesoftware.code-spell-checker
+      # vscode-extensions.pylyzer.pylyzer
+      # vscode-extensions.redhat.java
+      # vscode-extensions.rust-lang.rust-analyzer
+      # vscode-extensions.shd101wyy.markdown-preview-enhanced
+      # vscode-extensions.streetsidesoftware.code-spell-checker
       vscode-extensions.tamasfe.even-better-toml
       vscode-extensions.unifiedjs.vscode-mdx
       vscode-extensions.usernamehw.errorlens
-      vscode-extensions.vadimcn.vscode-lldb
-      vscode-extensions.vscjava.vscode-java-pack
+      # vscode-extensions.vadimcn.vscode-lldb
+      # vscode-extensions.vscjava.vscode-java-pack
       vscode-extensions.vue.volar
       vscode-extensions.vue.vscode-typescript-vue-plugin
       vscode-extensions.wakatime.vscode-wakatime
@@ -55,5 +53,11 @@
       vscode-extensions.yzane.markdown-pdf
       vscode-extensions.yzhang.markdown-all-in-one
     ];
+    # userSettings = {
+    #   prettier.documentSelectors = [ "**/*.astro" ];
+    #   "[astro]" = {
+    #     editor.defaultFormatter = "esbenp.prettier-vscode";
+    #   };
+    # };
   };
 }
