15c0585814be3077d9b4da5f529a44fc7527d63b
atp-gh <atp@disroot.org>
2025-03-05 20:51:00 +0800

    fix: Fix flake

--- flake.nix
+++ flake.nix
@@ -7,21 +7,25 @@
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    # home-manager.url = "github:nix-community/home-manager/master";
-    # home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    # stylix.url = "github:danth/stylix";
+    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    stylix.url = "github:danth/stylix";
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
     {
       nixpkgs,
-      # nixvim,
-      # home-manager,
+      nixvim,
+      home-manager,
       ...
     }@inputs:
     let
       system = "x86_64-linux";
-      host = "chromebook";
+      host = "asusbook";
       username = "atp";
     in
     {
@@ -36,19 +40,21 @@
           modules = [
             ./hosts/${host}/config.nix
             inputs.disko.nixosModules.disko
-            # inputs.stylix.nixosModules.stylix
-            # home-manager.nixosModules.home-manager
-            # {
-            # home-manager.extraSpecialArgs = {
-            #   inherit username;
-            #   inherit inputs;
-            #   inherit host;
-            # };
-            # home-manager.useGlobalPkgs = true;
-            # home-manager.useUserPackages = true;
-            # home-manager.backupFileExtension = "backup";
-            # home-manager.users.${username} = import ./hosts/${host}/home.nix;
-            # }
+            inputs.stylix.nixosModules.stylix
+            nixvim.nixosModules.nixvim
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.extraSpecialArgs = {
+                inherit username;
+                inherit inputs;
+                inherit host;
+                inherit nixvim;
+              };
+              # home-manager.useGlobalPkgs = true;
+              # home-manager.useUserPackages = true;
+              home-manager.backupFileExtension = "backup";
+              home-manager.users.${username} = import ./hosts/${host}/home.nix;
+            }
           ];
         };
       };
