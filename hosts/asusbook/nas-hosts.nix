853c5a24a7decac8444b47fc21a31d270d0009fc
atp-gh <atp@disroot.org>
2025-03-15 21:50:03 +0800

    feat: Add jupyter notebook

--- hosts/asusbook/nas-hosts.nix
+++ hosts/asusbook/nas-hosts.nix
@@ -1,15 +1,15 @@
 {
   networking.extraHosts = ''
-    192.168.6.173 0pt.us.kg
-    192.168.6.173 alist.0pt.us.kg
-    192.168.6.173 code.0pt.us.kg
-    192.168.6.173 dashboard.0pt.us.kg
-    192.168.6.173 git.0pt.us.kg
-    192.168.6.173 glances.0pt.us.kg
-    192.168.6.173 gotify.0pt.us.kg
-    192.168.6.173 ntfy.0pt.us.kg
-    192.168.6.173 pic.0pt.us.kg
-    192.168.6.173 sync.0pt.us.kg
-    192.168.6.173 wakapi.0pt.us.kg
+    192.168.6.173 0pt.icu
+    192.168.6.173 alist.0pt.icu
+    192.168.6.173 code.0pt.icu
+    192.168.6.173 dashboard.0pt.icu
+    192.168.6.173 git.0pt.icu
+    192.168.6.173 glances.0pt.icu
+    192.168.6.173 gotify.0pt.icu
+    192.168.6.173 ntfy.0pt.icu
+    192.168.6.173 pic.0pt.icu
+    192.168.6.173 sync.0pt.icu
+    192.168.6.173 wakapi.0pt.icu
   '';
 }
--- modules/service/jupyter.nix
+++ modules/service/jupyter.nix
@@ -1,10 +1,45 @@
+{ pkgs, ... }:
 {
   services.jupyter = {
-    enable = false;
+    enable = true;
     command = "jupyter notebook";
     ip = "127.0.0.1";
     port = 8888;
     user = "atp";
     group = "users";
+    password = "argon2:$argon2id$v=19$m=10240,t=10,p=8$AsKAIexdBAxO5vimZYyCyA$fCN4QSJpaMAWHG7GUMIJHdxu0W9uXgEPh7jfVXoHZEQ";
+    # extraPackages = [
+    #   pkgs.python3.pkgs.requests
+    #   pkgs.python3.pkgs.numpy
+    #   pkgs.python3.pkgs.nbconvert
+    #   pkgs.python3.pkgs.playwright
+    # ];
+    kernels = {
+      python3 =
+        let
+          env = (
+            pkgs.python3.withPackages (
+              pythonPackages: with pythonPackages; [
+                ipykernel
+                pandas
+                scikit-learn
+                requests
+                numpy
+              ]
+            )
+          );
+        in
+        {
+          displayName = "Python 3 for machine learning";
+          argv = [
+            "${env.interpreter}"
+            "-m"
+            "ipykernel_launcher"
+            "-f"
+            "{connection_file}"
+          ];
+          language = "python";
+        };
+    };
   };
 }
