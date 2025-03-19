cc4acac731d952f253088cbecf497ff7583e1a58
atp-gh <atp@disroot.org>
2025-03-19 19:34:51 +0800

    feat: Disabling automatic core dumps

--- modules/system/base.nix
+++ modules/system/base.nix
@@ -45,6 +45,7 @@ in
     # Needed For Some Steam Games
     kernel.sysctl = {
       "vm.max_map_count" = 2147483642;
+      "kernel.core_pattern" = "|/bin/false";
     };
     # Bootloader.
     loader = {
