beb660841535bc7dbb3bc3aedaf43ac0c6d34431
atp-gh <atp@disroot.org>
2025-03-24 22:43:12 +0800

    refactor: Delete useless code because yazi is already intergrated with nushell

--- home/cli/nushell.nix
+++ home/cli/nushell.nix
@@ -101,15 +101,6 @@
         prepend /home/myuser/.apps |
         append /usr/bin/env
         )
-        def --env y [...args] {
-        	let tmp = (mktemp -t "yazi-cwd.XXXXXX")
-        	yazi ...$args --cwd-file $tmp
-        	let cwd = (open $tmp)
-        	if $cwd != "" and $cwd != $env.PWD {
-        		cd $cwd
-        	}
-        	rm -fp $tmp
-        }
       '';
     };
     carapace.enable = true;
