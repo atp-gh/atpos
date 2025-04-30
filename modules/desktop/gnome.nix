cd58c11692e1cd3be00ac4c7edd5687e903fc9b6
atp-gh <atp@disroot.org>
2025-04-30 10:35:47 +0800

    feat: Add gnome desktop manager

---
+++ modules/desktop/gnome.nix
@@ -0,0 +1,28 @@
+{
+  host,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  inherit (import ../../hosts/${host}/env.nix) DM;
+in
+lib.mkIf (DM == "Gnome") {
+  services = {
+    xserver = {
+      enable = true;
+      displayManager.gdm.enable = true;
+      desktopManager.gnome.enable = true;
+      ## Remove XTerm
+      excludePackages = [ pkgs.xterm ];
+    };
+    gnome.core-utilities.enable = false;
+  };
+
+  environment.gnome.excludePackages = with pkgs; [
+    gnome-backgrounds
+    gnome-tour
+    gnome-user-docs
+  ];
+  services.power-profiles-daemon.enable = lib.mkForce false;
+}
