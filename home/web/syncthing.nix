a5f0f20fccf17fae53678273fe8e751c63554ef0
atp-gh <atp@disroot.org>
2025-04-30 10:42:05 +0800

    feat: Add option for syncthing

--- home/web/syncthing.nix
+++ home/web/syncthing.nix
@@ -1,4 +1,8 @@
-{
+{ host, lib, ... }:
+let
+  inherit (import ../../hosts/${host}/env.nix) Syncthing;
+in
+lib.mkIf (Syncthing) {
   services.syncthing = {
     enable = true;
     settings = {
@@ -6,16 +10,12 @@
         Nas = {
           id = "6NZ5BBU-VEZ3UBW-I5XGSHU-NJSJSND-ASBOOK4-SQ5GUXK-T3HKDWS-2UTTEQ6";
         };
-        PostmarketOS = {
-          id = "OP5TTKB-AI2JABB-6YBI2Q4-XHHFAQQ-QHO23VV-5QZBMXW-5MAM42F-BX74AQZ";
-        };
       };
       folders = {
         "/home/atp/Sync" = {
           id = "default";
           devices = [
             "Nas"
-            "PostmarketOS"
           ];
           versioning.type = "trashcan";
         };
