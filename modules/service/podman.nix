9172fa9622892e7ac455a112dcea95db6b9351d1
atp-gh <atp@disroot.org>
2025-05-02 20:55:33 +0800

    chore: Shut down podman

--- modules/service/podman.nix
+++ modules/service/podman.nix
@@ -1,8 +1,8 @@
 {
-  virtualisation.containers.enable = true;
+  virtualisation.containers.enable = false;
   virtualisation = {
     podman = {
-      enable = true;
+      enable = false;
       # Create a `docker` alias for podman, to use it as a drop-in replacement
       dockerCompat = true;
       # Required for containers under podman-compose to be able to talk to each other.
