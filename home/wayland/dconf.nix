17579f2da38491340424792870bdc087e7bfbb65
atp-gh <atp@disroot.org>
2025-05-02 23:42:46 +0800

    fix: Correct syntax error

--- home/wayland/dconf.nix
+++ home/wayland/dconf.nix
@@ -1,12 +1,13 @@
-{pkgs,...}: {
+{pkgs, ...}: {
   dconf = {
     enable = true;
-    settings={
+    settings = {
       "org/gnome/shell" = {
         disable-user-extensions = false; # enables user extensions
         enabled-extensions = [
           pkgs.gnomeExtensions.gsconnect.extensionUuid
         ];
+      };
     };
   };
 }
