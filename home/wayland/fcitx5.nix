fbe7fee5cf084476a66af03f004a0886cbf07f47
atp-gh <atp@disroot.org>
2025-05-03 11:01:14 +0800

    feat: Disable fcitx5 in gnome

--- home/wayland/fcitx5.nix
+++ home/wayland/fcitx5.nix
@@ -1,14 +1,22 @@
-{pkgs, ...}: {
-  i18n.inputMethod = {
-    enable = true;
-    type = "fcitx5";
-    fcitx5.addons = with pkgs; [
-      fcitx5-configtool # needed enable rime using configtool after installed
-      fcitx5-chinese-addons
-      fcitx5-material-color # theme
-      fcitx5-gtk # gtk im module
-      fcitx5-rime # for flypy chinese input method
-      libsForQt5.fcitx5-qt # qt im module
-    ];
-  };
-}
+{
+  host,
+  lib,
+  pkgs,
+  ...
+}: let
+  inherit (import ../../hosts/${host}/env.nix) WM;
+in
+  lib.mkIf (WM == "Hyprland" || WM == "niri") {
+    i18n.inputMethod = {
+      enable = true;
+      type = "fcitx5";
+      fcitx5.addons = with pkgs; [
+        fcitx5-configtool # needed enable rime using configtool after installed
+        fcitx5-chinese-addons
+        fcitx5-material-color # theme
+        fcitx5-gtk # gtk im module
+        fcitx5-rime # for flypy chinese input method
+        libsForQt5.fcitx5-qt # qt im module
+      ];
+    };
+  }
