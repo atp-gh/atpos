f6b857010c1d3cfef865b91b945840d315bacf40
atp-gh <atp@disroot.org>
2025-05-03 11:01:59 +0800

    feat: Add screen autorotate in gnome

--- home/wayland/dconf.nix
+++ home/wayland/dconf.nix
@@ -1,13 +1,24 @@
-{pkgs, ...}: {
-  dconf = {
-    enable = true;
-    settings = {
-      "org/gnome/shell" = {
-        disable-user-extensions = false; # enables user extensions
-        enabled-extensions = [
-          pkgs.gnomeExtensions.gsconnect.extensionUuid
-        ];
+{
+  host,
+  lib,
+  pkgs,
+  ...
+}: let
+  inherit (import ../../hosts/${host}/env.nix) DM;
+in
+  lib.mkIf (DM == "Gnome") {
+    home.packages = with pkgs.gnomeExtensions; [
+      screen-rotate
+    ];
+    dconf = {
+      enable = true;
+      settings = {
+        "org/gnome/shell" = {
+          disable-user-extensions = false; # enables user extensions
+          enabled-extensions = with pkgs.gnomeExtensions; [
+            screen-rotate.extensionUuid
+          ];
+        };
       };
     };
-  };
-}
+  }
