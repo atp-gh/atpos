0bd57d710cf07e4e81a3c29955d415da66077250
atp-gh <atp@disroot.org>
2025-06-20 23:04:15 +0800

    fix: Fix fcitx5

--- home/wayland/fcitx5.nix
+++ home/wayland/fcitx5.nix
@@ -10,13 +10,26 @@ in
     i18n.inputMethod = {
       enable = true;
       type = "fcitx5";
-      fcitx5.addons = with pkgs; [
-        fcitx5-configtool # needed enable rime using configtool after installed
-        fcitx5-chinese-addons
-        fcitx5-material-color # theme
-        fcitx5-gtk # gtk im module
-        fcitx5-rime # for flypy chinese input method
-        libsForQt5.fcitx5-qt # qt im module
-      ];
+      fcitx5 = {
+        addons = with pkgs; [
+          fcitx5-configtool # needed enable rime using configtool after installed
+          fcitx5-chinese-addons
+          fcitx5-material-color # theme
+          fcitx5-gtk # gtk im module
+          fcitx5-rime # for flypy chinese input method
+          libsForQt5.fcitx5-qt # qt im module
+        ];
+        settings.inputMethod = {
+          GroupOrder."0" = "Default";
+          "Groups/0" = {
+            Name = "Default";
+            "Default Layout" = "us";
+            DefaultIM = "pinyin";
+          };
+          "Groups/0/Items/0".Name = "keyboard-us";
+          "Groups/0/Items/1".Name = "pinyin";
+        };
+        waylandFrontend = true;
+      };
     };
   }
