ce5b12bb5ea78bc30a9d19e922d94c8c29d53a97
atp-gh <atp@disroot.org>
2025-08-25 21:10:05 +0800

    feat: Delete some packages

--- modules/system/environment.nix
+++ modules/system/environment.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}: {
   environment.systemPackages = with pkgs; [
-    # archive
+    # Archive
     p7zip
     gnutar
     unzipNLS
@@ -8,36 +8,23 @@
     zip
     zstd
 
-    # core
-    tuigreet
-    just
-    v4l-utils # For OBS virtual cam support
-
-    # editor use in tty
-    micro
-
     # networking tool
     curl
     wget
 
     # misc
-    expect
-    file
-    gcc
-    ghc
-    gnumake
-    jq
     libvirt
     lm_sensors
     libnotify
-    meson
-    ninja
-    pkg-config
-    zenith
+    v4l-utils # For OBS virtual cam support
   ];
   programs = {
     dconf.enable = true;
     fuse.userAllowOther = true;
+    git = {
+      enable = true;
+      package = pkgs.gitMinimal;
+    };
     mtr.enable = true;
     sniffnet.enable = true;
     ssh.startAgent = true;
