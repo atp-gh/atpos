20ef221ef4f822ad4b4c6ddc8ab2066765647212
atp-gh <atp@disroot.org>
2025-08-27 15:13:35 +0800

    style: Just format

--- home/cli/nushell.nix
+++ home/cli/nushell.nix
@@ -1,10 +1,7 @@
-{
+_: {
   programs = {
     nushell = {
       enable = true;
-      # The config.nu can be anywhere you want if you like to edit your Nushell with Nu
-      # configFile.source = ./.../config.nu;
-      # for editing directly to config.nu
       extraConfig = ''
         let carapace_completer = {|spans|
             carapace $spans.0 nushell ...$spans | from json
@@ -103,7 +100,9 @@
         )
       '';
     };
-    carapace.enable = true;
-    carapace.enableNushellIntegration = true;
+    carapace = {
+      enable = true;
+      enableNushellIntegration = true;
+    };
   };
 }
