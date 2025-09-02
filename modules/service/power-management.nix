37ecdd3e456be0ca32619922ef211f5701db383b
atp-gh <atp@disroot.org>
2025-09-02 10:10:11 +0800

    chore: Change powermanager

--- modules/service/power-management.nix
+++ modules/service/power-management.nix
@@ -1,27 +1,27 @@
 {
-  # powerManagement = {
-  #   enable = true;
-  #   cpuFreqGovernor = "performance";
-  # };
-  services = {
-    thermald.enable = false;
-    tlp = {
-      enable = false;
-      settings = {
-        CPU_SCALING_GOVERNOR_ON_AC = "performance";
-        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-
-        CPU_ENERGY_PERF_POLICY_ON_BAT = "powersave";
-        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-
-        CPU_MIN_PERF_ON_AC = 0;
-        CPU_MAX_PERF_ON_AC = 100;
-        CPU_MIN_PERF_ON_BAT = 0;
-        CPU_MAX_PERF_ON_BAT = 80;
-
-        START_CHARGE_THRESH_BAT0 = 40;
-        STOP_CHARGE_THRESH_BAT0 = 80;
-      };
-    };
+  powerManagement = {
+    enable = true;
+    cpuFreqGovernor = "performance";
   };
+  # services = {
+  #   thermald.enable = false;
+  #   tlp = {
+  #     enable = false;
+  #     settings = {
+  #       CPU_SCALING_GOVERNOR_ON_AC = "performance";
+  #       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+  #
+  #       CPU_ENERGY_PERF_POLICY_ON_BAT = "powersave";
+  #       CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+  #
+  #       CPU_MIN_PERF_ON_AC = 0;
+  #       CPU_MAX_PERF_ON_AC = 100;
+  #       CPU_MIN_PERF_ON_BAT = 0;
+  #       CPU_MAX_PERF_ON_BAT = 80;
+  #
+  #       START_CHARGE_THRESH_BAT0 = 40;
+  #       STOP_CHARGE_THRESH_BAT0 = 80;
+  #     };
+  #   };
+  # };
 }
