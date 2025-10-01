f1571943878edf98a740fe10986c217e61969b10
atp-gh <atp@disroot.org>
2025-10-01 18:47:25 +0800

    chore: Recovery laptop and desktop power policy

--- modules/service/power-management.nix
+++ modules/service/power-management.nix
@@ -4,30 +4,30 @@
   ...
 }: let
   inherit (import ../../hosts/${host}/env.nix) Platform;
-in {
-  powerManagement = lib.mkIf (Platform == "desktop") {
-    enable = true;
-    cpuFreqGovernor = "performance";
-  };
-  services = lib.mkIf (Platform == "laptop") {
-    thermald.enable = true;
-    tlp = {
+in
+  with lib; {
+    powerManagement = mkIf (Platform == "desktop") {
       enable = true;
-      settings = {
-        CPU_SCALING_GOVERNOR_ON_AC = "performance";
-        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      cpuFreqGovernor = "performance";
+    };
+    services = mkIf (Platform == "laptop") {
+      tlp = {
+        enable = true;
+        settings = {
+          CPU_SCALING_GOVERNOR_ON_AC = "performance";
+          CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
-        CPU_ENERGY_PERF_POLICY_ON_BAT = "powersave";
-        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+          CPU_ENERGY_PERF_POLICY_ON_BAT = "powersave";
+          CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
 
-        CPU_MIN_PERF_ON_AC = 0;
-        CPU_MAX_PERF_ON_AC = 100;
-        CPU_MIN_PERF_ON_BAT = 0;
-        CPU_MAX_PERF_ON_BAT = 80;
+          CPU_MIN_PERF_ON_AC = 0;
+          CPU_MAX_PERF_ON_AC = 100;
+          CPU_MIN_PERF_ON_BAT = 0;
+          CPU_MAX_PERF_ON_BAT = 80;
 
-        # START_CHARGE_THRESH_BAT0 = 40;
-        # STOP_CHARGE_THRESH_BAT0 = 100;
+          START_CHARGE_THRESH_BAT0 = 40;
+          STOP_CHARGE_THRESH_BAT0 = 80;
+        };
       };
     };
-  };
-}
+  }
