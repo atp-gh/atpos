a17ab933aa8c3939105a76bb970967c6816d4614
atp-gh <atp@disroot.org>
2025-10-13 21:45:30 +0800

    feat: Replace nix format for helix

--- home/tui/helix.nix
+++ home/tui/helix.nix
@@ -16,7 +16,7 @@
       {
         name = "nix";
         auto-format = true;
-        formatter.command = lib.getExe pkgs.nixfmt-rfc-style;
+        formatter.command = lib.getExe pkgs.alejandra;
       }
     ];
   };
