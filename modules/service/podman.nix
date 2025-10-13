0e912fed4367dc0c8b8f632e63bf26b63208ecf3
atp-gh <atp@disroot.org>
2025-10-13 21:50:11 +0800

    feat: Update podman config

--- modules/service/podman.nix
+++ modules/service/podman.nix
@@ -1,17 +1,46 @@
-{
-  virtualisation.containers.enable = false;
+_: {
   virtualisation = {
+    containers = {
+      enable = false;
+      containersConf.settings = {
+        containers.dns_servers = [
+          "185.222.222.222"
+          "45.11.45.11"
+          "9.9.9.9"
+          "149.112.112.112"
+          "2a0d:2a00:1::2"
+          "2a0d:2a00:2::2"
+        ];
+      };
+      storage.settings = {
+        storage.driver = "zfs";
+        storage.graphroot = "/var/lib/containers/storage";
+        storage.runroot = "/run/containers/storage";
+        storage.options.zfs.fsname = "zroot/root";
+      };
+    };
     podman = {
       enable = false;
-      # Create a `docker` alias for podman, to use it as a drop-in replacement
       dockerCompat = true;
-      # Required for containers under podman-compose to be able to talk to each other.
-      defaultNetwork.settings.dns_enabled = true;
+      autoPrune = {
+        enable = true;
+        dates = "weekly";
+      };
+      defaultNetwork.settings = {
+        dns_enabled = true;
+        ipv6_enabled = true;
+        subnets = [
+          {
+            subnet = "10.88.0.0/16";
+            gateway = "10.88.0.1";
+          }
+          {
+            subnet = "fd00:db8::/64";
+            gateway = "fd00:db8::1";
+          }
+        ];
+      };
     };
-  };
-  virtualisation.oci-containers.backend = "podman";
-  virtualisation.oci-containers.containers = {
-    # alist = import ./docker/alist.nix;
-    # wakapi = import ./docker/wakapi.nix;
+    oci-containers.backend = "podman";
   };
 }
