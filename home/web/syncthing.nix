6e6dc10b98076bbd93a10b05757975fb182d9463
atp-gh <atp@disroot.org>
2025-11-20 14:51:23 +0800

    feat: Enable syncthing without config

--- home/web/syncthing.nix
+++ home/web/syncthing.nix
@@ -8,26 +8,5 @@ in
   lib.mkIf Syncthing {
     services.syncthing = {
       enable = true;
-      settings = {
-        devices = {
-          homelab = {
-            id = "5H7XA3F-R5WFPON-DV57OGC-M6RA3FF-UHZBT3M-TYD4MNH-YR3KZGW-TDE5RQZ";
-          };
-        };
-        folders = {
-          "/home/atp/Sync" = {
-            id = "default";
-            devices = [
-              "homelab"
-            ];
-            versioning.type = "trashcan";
-          };
-        };
-        options = {
-          globalAnnounceEnabled = false;
-          localAnnounceEnabled = true;
-          relaysEnabled = false;
-        };
-      };
     };
   }
