40999592d0ff1ded9f421a97afeceffc06b6535e
atp-gh <atp@disroot.org>
2026-03-04 22:45:51 +0800

    chore: Change nas ip

--- hosts/icebreaker/nas-hosts.nix
+++ hosts/icebreaker/nas-hosts.nix
@@ -2,7 +2,7 @@ _: {
   services.unbound.settings.server = {
     local-zone = "'0pt.lab.' redirect";
     local-data = [
-      "'0pt.lab. IN A 192.168.6.173'"
+      "'0pt.lab. IN A 192.168.6.249'"
     ];
   };
 }
