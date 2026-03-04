a3e548144bbf98347fe815bfbca953fda79e6fba
atp-gh <atp@disroot.org>
2026-03-04 22:46:40 +0800

    chore: Switch to unbound as dns resolver

--- modules/system/network.nix
+++ modules/system/network.nix
@@ -3,38 +3,56 @@
   networking = {
     dhcpcd.extraConfig = "nohook resolv.conf";
     networkmanager = {
-      dns = "systemd-resolved";
+      dns = "none";
       enable = true;
     };
     resolvconf.enable = lib.mkForce false;
   };
 
   # Set Encrypted_DNS
-  services.resolved = {
+  services.unbound = {
     enable = true;
     settings = {
-      Resolve = {
-        DNSOverTLS = "opportunistic";
-        DNSSEC = true;
-        Domains = ["~."];
-        FallbackDNS = [
-          # CloudFlare servers
-          "1.1.1.1#cloudflare-dns.com"
-          "1.0.0.1#cloudflare-dns.com"
-          "2606:4700:4700::1111#cloudflare-dns.com"
-          "2606:4700:4700::1001#cloudflare-dns.com"
-          # dns.sb servers
-          "185.222.222.222#dot.sb"
-          "45.11.45.11#dot.sb"
-          "2a09::#dot.sb"
-          "2a11::#dot.sb"
-          # Quad9 servers
-          "9.9.9.10#dns.quad9.net"
-          "149.112.112.10#dns.quad9.net"
-          "2620:fe::10#dns.quad9.net"
-          "2620:fe::fe:10#dns.quad9.net"
+      server = {
+        do-ip4 = true;
+        do-ip6 = true;
+        do-tcp = true;
+        do-udp = true;
+        hide-identity = true;
+        hide-version = true;
+        interface = [
+          "127.0.0.1"
+          "::1"
         ];
+        num-threads = 2;
+        prefetch = true;
+        qname-minimisation = true;
+        use-syslog = true;
+        verbosity = 1;
       };
+      forward-zone = [
+        {
+          forward-addr = [
+            # cloudflare servers
+            "1.1.1.1@853#cloudflare-dns.com"
+            "1.0.0.1@853#cloudflare-dns.com"
+            "2606:4700:4700::1111@853#cloudflare-dns.com"
+            "2606:4700:4700::1001@853#cloudflare-dns.com"
+            # dns.sb servers
+            "185.222.222.222@853#dot.sb"
+            "45.11.45.11@853#dot.sb"
+            "2a09::@853#dot.sb"
+            "2a11::@853#dot.sb"
+            # quad9 servers
+            "9.9.9.9@853#dns.quad9.net"
+            "149.112.112.112@853#dns.quad9.net"
+            "2620:fe::fe@853#dns.quad9.net"
+            "2620:fe::9@853#dns.quad9.net"
+          ];
+          forward-tls-upstream = true;
+          name = ".";
+        }
+      ];
     };
   };
 }
